8e625f77c2985c2a928782094b9b61085fa0426a
Brenda Pérez <brendaperez@MacBook-Air-de-Brenda.local>
2017-10-09 13:16:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brendaperez/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.brendaperez;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.brendaperez.math4us.MainActivity;
+import com.example.brendaperez.math4us.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
