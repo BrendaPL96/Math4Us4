f65d06da6d5f7f0e0bec43886a08ca3ec8ba323b
Thomas <thomasbh@users.noreply.github.com>
2017-10-25 13:41:13 -0500

    Restoring app

--- app/src/main/java/com/example/brendaperez/math4us/MenuPrincipal.java
+++ app/src/main/java/com/example/brendaperez/math4us/MenuPrincipal.java
@@ -1,8 +1,8 @@
 package com.example.brendaperez.math4us;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.brendaperez.Categorias;
