3b5d3be5b5ff894c7771c90aa2ac9aab56f6edc0
Thomas <thomasbh@users.noreply.github.com>
2017-10-25 14:53:07 -0500

    Counting bees, and it's working !

--- app/src/main/java/udlap/ingdesoft/math4us/Level1_1.java
+++ app/src/main/java/udlap/ingdesoft/math4us/Level1_1.java
@@ -1,8 +1,12 @@
 package udlap.ingdesoft.math4us;
 
+import android.animation.Animator;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -40,6 +44,58 @@ public class Level1_1 extends AppCompatActivity {
         digitRight = rightSum.getChildCount();
         total = digitLeft + digitRight;
         return total;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    protected void animate(View v) {
+
+        // get the center for the clipping circle
+        int cx = v.getWidth() / 2;
+        int cy = v.getHeight() / 2;
+
+        // get the final radius for the clipping circle
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+        // create the animator for this view (the start radius is zero)
+        Animator anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, finalRadius);
+        anim.start();
+
+    }
+
+    protected void onClickAll(View v) {
+        CharSequence numberText = v.getContentDescription();
+        int number = Integer.parseInt(numberText.toString());
+        int id = v.getId();
+        ImageView image = (ImageView) findViewById(id);
+        if (number == countBee()) {
+            image.setImageResource(R.drawable.right);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                animate(five);
+            }
+            if (three.isClickable()) {
+                three.setImageResource(R.drawable.wrong);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    animate(three);
+                }
+            }
+            if (one.isClickable()) {
+                one.setImageResource(R.drawable.wrong);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    animate(one);
+                }
+            }
+            question.setVisibility(View.GONE);
+
+            bravo.setVisibility(View.VISIBLE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                animate(bravo);
+            }
+            five.setClickable(false);
+        } else {
+            image.setImageResource(R.drawable.wrong);
+            image.setClickable(false);
+
+        }
     }
 
 
