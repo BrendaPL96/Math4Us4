48f3a7a13ebedd8b8fb06da481de61ade92fd961
Thomas <thomasbh@users.noreply.github.com>
2017-10-25 13:56:03 -0500

    Cambios sencillos

--- app/src/main/java/com/example/brendaperez/NivelSuma.java
+++ app/src/main/java/com/example/brendaperez/NivelSuma.java
@@ -1,8 +1,8 @@
 package com.example.brendaperez;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.brendaperez.math4us.R;
@@ -17,7 +17,7 @@ public class NivelSuma extends AppCompatActivity {
         setContentView(R.layout.nivelsuma);
     }
 
-    public void cambio6 (View view){
+    public void goLvl1(View view) {
         Intent objI = new Intent(NivelSuma.this, Level1_1.class);
         startActivity(objI);
     }
--- app/src/main/java/com/example/brendaperez/SumasyRestas.java
+++ app/src/main/java/com/example/brendaperez/SumasyRestas.java
@@ -1,11 +1,10 @@
 package com.example.brendaperez;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.example.brendaperez.math4us.MenuPrincipal;
 import com.example.brendaperez.math4us.R;
 
 public class SumasyRestas extends AppCompatActivity {
@@ -15,7 +14,10 @@ public class SumasyRestas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sumasyrestas);
     }
-    public void cambio7 (View view){
+
+    public void goSuma(View view) {
+
         Intent objI = new Intent(SumasyRestas.this,NivelSuma.class);
+        startActivity(objI);
     }
 }
