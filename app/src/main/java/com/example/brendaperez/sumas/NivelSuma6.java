5719c44bac953f075168a40e5a1e61bad9c25e9a
Thomas <thomasbh@users.noreply.github.com>
2017-10-31 20:14:09 -0600

    Nivel 6 v2 (2 columnas)

--- app/src/main/java/com/example/brendaperez/sumas/NivelSuma6.java
+++ app/src/main/java/com/example/brendaperez/sumas/NivelSuma6.java
@@ -1,6 +1,7 @@
 package com.example.brendaperez.sumas;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.brendaperez.math4us.R;
 
@@ -11,5 +12,20 @@ public class NivelSuma6 extends NivelSuma {
         setContentView(R.layout.activity_nivel_suma_6);
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    protected int countPic() {
+        LinearLayout leftSum2 = findViewById(R.id.leftSum2);
+        digitLeft = leftSum2.getChildCount() + leftSum.getChildCount();
+        LinearLayout rightSum2 = findViewById(R.id.rightSum2);
+        digitRight = rightSum.getChildCount() + rightSum2.getChildCount();
+        total = digitLeft + digitRight;
+        // generating the congratulation text
+        bravoSumStatic = getResources().getString(R.string.bravoSum);
+        bravoSum = String.format(bravoSumStatic, digitLeft, digitRight, total);
+        bravo.setText(bravoSum);
+        //returning the total
+        return total;
     }
 }
