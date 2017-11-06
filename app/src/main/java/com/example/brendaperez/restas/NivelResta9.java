c06dd65959b2c4af0c2dfff852fee1987ea56d7e
Thomas <thomasbh@users.noreply.github.com>
2017-11-06 13:38:07 -0600

    nivel 9

---
+++ app/src/main/java/com/example/brendaperez/restas/NivelResta9.java
@@ -0,0 +1,14 @@
+package com.example.brendaperez.restas;
+
+import android.os.Bundle;
+
+import com.example.brendaperez.math4us.R;
+
+public class NivelResta9 extends NivelResta {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_nivel_resta_6);
+        super.onCreate(savedInstanceState);
+    }
+}
