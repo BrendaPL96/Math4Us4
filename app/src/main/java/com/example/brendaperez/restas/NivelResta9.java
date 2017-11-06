e2af5470c932f5162855a4e1c10757b78b18b974
Thomas <thomasbh@users.noreply.github.com>
2017-11-06 13:48:42 -0600

    nivel 9

--- app/src/main/java/com/example/brendaperez/restas/NivelResta9.java
+++ app/src/main/java/com/example/brendaperez/restas/NivelResta9.java
@@ -1,6 +1,7 @@
 package com.example.brendaperez.restas;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.brendaperez.math4us.R;
 
@@ -8,7 +9,22 @@ public class NivelResta9 extends NivelResta {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_nivel_resta_6);
+        setContentView(R.layout.activity_nivel_resta_9);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int countPic() {
+        LinearLayout leftRest2 = findViewById(R.id.leftRest2);
+        digitLeft = leftRest2.getChildCount() + leftRest.getChildCount();
+        LinearLayout rightRest2 = findViewById(R.id.rightRest2);
+        digitRight = rightRest.getChildCount() + rightRest2.getChildCount();
+        total = digitLeft + digitRight;
+        // generating the congratulation text
+        bravoRestStatic = getResources().getString(R.string.bravoRest);
+        bravoRest = String.format(bravoRestStatic, digitLeft, digitRight, total);
+        bravo.setText(bravoRest);
+        //returning the total
+        return total;
     }
 }
